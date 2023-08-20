4e8f35936359389187ebe1eb501e9c7e983ca821
Yash Gupta <yashgupta1249@gmail.com>
2023-08-20 20:14:16 +0530

    Implement negative number check for a factorial function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int main(){
     print_hello();
     cout<< endl;
-    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    int n;
+    while(true){
+        cout<<"Enter a positive number: ";
+        cin>>n;
+        int res = factorial(n);
+        if(res==-1){
+            cout<<"Entered number is negative."<<"\n";
+        }else{
+            cout<<"The factorial of "<<n<<" is "<< res << endl;
+            break;
+        }
+    }
     return 0;
 }
